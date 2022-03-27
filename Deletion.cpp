ccca9629283aa32540218dc063a23e0cd9213742
Monna4335 <79435214+Monna4335@users.noreply.github.com>
2022-03-27 15:54:35 +0600

    Create Deletion.cpp

---
+++ Deletion.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+class dellete{
+public:
+    int size,array[100];
+    dellete (int n,int b[])
+    {
+        size=n;
+        for(int i=0;i<n;i++)array[i]=b[i];
+    }
+    void del(int index)
+    {
+        for(int i=index;i<size;i++)
+        {
+            array[i]=array[i+1];
+
+        }
+        size--;
+    }
+    void show()
+    {
+        for(int i=0;i<size;i++)cout<<array[i]<<" ";
+    }
+};
+int main()
+{
+    int n,a[1000],in,val,i,j,k;
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    cout<<"Enter the index to delete ";
+    cin>>in;
+    dellete A(n,a);
+    A.del(in);
+    A.show();
+}
