cf894110d5d9e740452312baf70da67beaee0e86
Monna4335 <79435214+Monna4335@users.noreply.github.com>
2022-03-27 15:48:01 +0600

    Update queue.cpp

--- queue.cpp
+++ queue.cpp
@@ -17,7 +17,7 @@ void queue::enqueue(int ind)
     if (isFull())
     {
         cout << "Queue is full\";
-        return;
+            return;
     }
     if (count == 0)
         rear = front = 0;
@@ -34,14 +34,12 @@ int queue::dequeue()
     if (isEmpty())
     {
         cout << "Queue is already empty\";
-        return -1;
+            return -1;
     }
     int val = qu[front];
     count--;
     if (count == 0)
         rear = front = -1;
-    else if (front == queuesize - 1)
-        front = 0;
     else
         front++;
 
@@ -67,7 +65,7 @@ void queue::traverse()
     if (isEmpty())
     {
         cout << "queue is empty\";
-        return;
+            return;
     }
     i = front;
     while (true)
@@ -75,8 +73,6 @@ void queue::traverse()
         cout << qu[i] << " ";
         if (i == rear)
             break;
-        else if (i == queuesize - 1)
-            i = 0;
         else
             i++;
     }
@@ -87,11 +83,20 @@ void queue::traverse()
 int main()
 {
     long long n, i, j, k, l;
-    queue p;
-    p.enqueue(67);
-    p.traverse();
-    p.enqueue(222);
-    p.traverse();
-    p.dequeue();
-    p.traverse();
-}
+    queue q;
+    while (1)
+    {
+        cout << "Enter 1 to enqueue,2 to dequeue,3 to traverse : ";
+        cin >> k;
+        if (k == 1)
+        {
+            cout << "Enter a value : ";
+            cin >> l;
+            q.enqueue(l);
+        }
+        else if (k == 2)
+            q.dequeue();
+            
+        else
+            q.traverse();
+    }
