22b23a573ec4ac089e5e2856da26fe61733e77f3
Monna4335 <79435214+Monna4335@users.noreply.github.com>
2022-03-27 15:42:32 +0600

    Create Insert.cpp

---
+++ Insert.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+class insert{
+public:
+    int size,array[100];
+    insert (int n,int b[])
+    {
+        size=n;
+        for(int i=0;i<n;i++)array[i]=b[i];
+    }
+    void inserrt(int index,int value)
+    {
+        for(int i=size-1;i>=index-1;i--)
+        {
+            array[i+1]=array[i];
+
+        }
+        array[index-1]=value;
+        size++;
+    }
+    void show()
+    {
+        for(int i=0;i<size;i++)cout<<array[i]<<" ";
+    }
+};
+int main()
+{
+    int n,a[1000],in,val,i,j,k;
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    cin>>in>>val;
+    insert A(n,a);
+    A.inserrt(in,val);
+    A.show();
+}
