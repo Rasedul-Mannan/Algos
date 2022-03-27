a605ddc5f0a81f4af652819003619faeece0cf51
Monna4335 <79435214+Monna4335@users.noreply.github.com>
2022-03-27 15:45:46 +0600

    Create Stack.cpp

---
+++ Stack.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+int stacksize = 100, st[100];
+class stack
+{
+  public:
+    int top = -1;
+    void push(int ind);
+    int pop();
+    bool isFull();
+    bool isEmpty();
+    void traverse();
+};
+void stack::push(int ind)
+{
+    if (isFull())
+    {
+        cout << "Stack Overflow\";
+        return;
+    }
+    top++;
+    st[top] = ind;
+    return;
+}
+int stack::pop()
+{
+    if (isEmpty())
+    {
+        cout << "Stack is already empty\";
+        return -1;
+    }
+    int val = st[top];
+    top--;
+    return val;
+}
+bool stack::isFull()
+{
+    if (top == stacksize - 1)
+        return true;
+    else
+        return false;
+}
+bool stack::isEmpty()
+{
+    if (top == -1)
+        return true;
+    else
+        return false;
+}
+void stack::traverse()
+{
+    int i;
+    if (isEmpty())
+        return;
+    for (i = 0; i <= top; i++)
+        cout << st[i] << " ";
+    cout << " Done\";
+}
+
+int main()
+{
+    long long n, i, j, k, l;
+    stack p;
+    p.push(67);
+    p.traverse();
+    p.push(56);
+    p.traverse();
+    p.pop();
+    p.traverse();
+}
